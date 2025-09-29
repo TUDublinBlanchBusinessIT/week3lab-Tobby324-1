7656b46a44ea1b7629293dd343851e9032d85176
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-09-29 16:37:10 +0100

    second change

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput,Button} from 'react-native';
 import React, {useState} from 'react';
+import Logo from './components/logo'
 
-
-export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
-  return(
-    <View> 
+  export default function App()
+ {function buttonClicked() {
+    //Alert.alert("button clicked"); //This works on a mobile phone
+    alert("button clicked"); // this works on the web version - try uncommenting one or the other lines as necessary
+}
+const {fullname, setFullname} = useState("Tobby");
+const [fname, setFname] = useState("Tobby");
+const [lname, setLname] = useState("Akinwale");
+const [dob, setDob] = useState("06 November 1998");
+  return (
+    <View >
+      <Logo/>
       <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+      <TextInput stylplaceholder="enter your name" onChangeText={setFullname}/>
+      <TextInput placeholder="Enter your firstname" onChangeText={setFname}/>
+      <TextInput placeholder="Enter your lastname" onChangeText={setLname}/>
+      <TextInput placeholder="Enter your date of birth" onChangeText={setDob}/>
+      <Button title="SUBMIT" onPress={buttonClicked}/>
     </View>
+  
   )
 };
-
-
-
